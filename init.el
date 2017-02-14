3467f7d3cc5078148fe3027d407a35ea67fe308f
Wen-Chun Lin <lovecankill@gmail.com>
2017-02-14 19:32:04 +0800

    Move custom file to specific directory

--- init.el
+++ init.el
@@ -1,4 +1,14 @@
 
+(defconst user-emacs-directory
+  (file-name-directory (or load-file-name (buffer-file-name)))
+  "My emacs config directory.")
+
+(defconst user-cache-directory
+  (file-name-as-directory (concat user-emacs-directory ".cache"))
+  "My emacs storage area for persistent files.")
+;; create the `user-cache-directory' if not exists
+(make-directory user-cache-directory t)
+
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
@@ -10,3 +20,8 @@
 
 (require 'pallet)
 (pallet-mode t)
+
+(setq-default custom-file (concat user-cache-directory "custom.el"))
+;; load custom-file only when file exist
+(when (file-exists-p custom-file)
+  (load-file custom-file))
