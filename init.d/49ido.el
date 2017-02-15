63bcd4131016a6b8cf3255fd1d8ee6b9a4297792
Wen-Chun Lin <lovecankill@gmail.com>
2017-02-15 15:46:54 +0800

    Add ido-vertical-mode and flx-ido

--- init.d/49ido.el
+++ init.d/49ido.el
@@ -4,6 +4,18 @@
 
   :config
   (define-key ido-file-completion-map "\C-w" 'backward-kill-word)
-  (ido-mode t))
+  (ido-mode t)
+  (ido-everywhere 1)
+
+  (use-package ido-vertical-mode
+    :config
+    (ido-vertical-mode 1)
+    (setq ido-vertical-define-keys 'C-n-C-p-up-and-down))
+
+  (use-package flx-ido
+    :config
+    (flx-ido-mode 1)
+    (setq ido-enable-flex-matching t)
+    (setq ido-use-face nil)))
 
 (provide '49ido)
