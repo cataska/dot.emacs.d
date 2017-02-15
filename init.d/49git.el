4b26bfe4791107923ba638f745754545632b7d30
Wen-Chun Lin <lovecankill@gmail.com>
2017-02-15 16:08:18 +0800

    Use bind-keys in use-package

--- init.d/49git.el
+++ init.d/49git.el
@@ -1,6 +1,7 @@
 (use-package magit
   :config
   (setq magit-have-graph 'set)
-  (global-set-key (kbd "C-x gs") 'magit-status))
+  (bind-keys :map global-map
+	     ("C-x gs" . magit-status)))
 
 (provide '49git)
